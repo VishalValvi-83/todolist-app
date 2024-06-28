dd7a6f26fd363e9edea6c8c783a4491da665273e
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-06-28 22:58:55 +0530

    new changes in UI

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './ToDoItem.css'
+import React from 'react';
+import './ToDoItem.css';
 
 function TodoItem({ toDoItem }) {
   return (
-    <div className="todo-item"> <p> <input type="checkbox" />{toDoItem.task}</p>
-      <span>Added on: {toDoItem.dateAdded} <br /> Due: {toDoItem.dueDate}</span>
+    <div className="todo-item">
+      <div className="checkbox-container">
+        <input type="checkbox" />
+      </div>
+      <div className="task-info">
+        <p>{toDoItem.task}</p>
+        <span>
+          <small>Added on: {toDoItem.dateAdded}</small> | 
+          <small> Due: {toDoItem.dueDate}</small>
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TodoItem
+export default TodoItem;
